934c412ff565cda23a36d7f9c3f17440657cea56
Amfiteatr42 <108868537+Amfiteatr42@users.noreply.github.com>
2022-12-24 07:49:15 +0200

    adds contacts routes

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,54 @@
-const express = require('express')
+const express = require("express");
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+  updateContact,
+} = require("../../models/contacts");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  const contacts = await listContacts();
+  res.set("Content-Type", "application/json").send(contacts);
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  const id = req.params.contactId;
+  const contact = await getContactById(id);
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  if (!contact) {
+    res.status(404).json({ message: "Not found" }).end();
+    return;
+  }
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json(contact);
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  const newContact = await addContact(req.body);
 
-module.exports = router
+  res.status(201).json(newContact);
+});
+
+router.delete("/:contactId", async (req, res, next) => {
+  const id = req.params.contactId;
+  const isContactExist = await removeContact(id);
+
+  isContactExist
+    ? res.status(200).json({ message: "Contact deleted" })
+    : res.status(404).json({ message: "Not found" });
+});
+
+router.put("/:contactId", async (req, res, next) => {
+  const id = req.params.contactId;
+  const contactChanges = req.body;
+  const updatedContact = await updateContact(id, contactChanges);
+
+  updatedContact
+    ? res.status(200).json(updatedContact)
+    : res.status(404).json({ message: "Not found" });
+});
+
+module.exports = router;
